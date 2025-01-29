f09d6910acd3852754b740e8d9b60f117324f2a9
Harshvardhan <harshvar8an@gmail.com>
2025-01-29 14:04:18 +0530

    added buttons route

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return(
     <div>
-      <p className="text-green-500">
-      Hello ling!!!
+      <p className="text-green-500 font-bold">
+      <Button>Hello ling!!!</Button>
       </p>
     </div>
   )
